0a4cbd03baa3e3ebdb805520c0ac143fee33afee
James Tupper (NWC) <tupper.james@gmail.com>
2015-04-13 16:49:19 -0500

    Add initial project files

---
+++ Olson.Test/Olson.Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Olson.Test.MvcApplication" Language="C#" %>
---
+++ Olson.Test/Olson.Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Olson.Test.Startup))]
+namespace Olson.Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
